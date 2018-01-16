4761504a4f75066e8ea7cb358b31779a1408eb20
Makoto Tanabe <xekid78@outlook.com>
2018-01-16 14:09:54 +0900

    サイコロの目をランダムに出力

---
+++ sample1.java
@@ -0,0 +1,8 @@
+public class sample1 {
+	public static void main(String[] args) {
+		double rand = Math.random() * 6 + 1;
+		int num = (int)rand;
+		System.out.println("サイコロの目は" + num + "です");
+	}
+
+}
