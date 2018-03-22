a4c99360ba3515b1483fe3041e15bf0c5f48f0c1
Makoto Tanabe <xekid78@outlook.com>
2018-03-22 11:37:43 +0900

    Delete Randomdice.java

--- Randomdice.java
+++
@@ -1,8 +0,0 @@
-public class Randomdice {
-	public static void main(String[] args) {
-		double rand = Math.random() * 6 + 1;
-		int num = (int)rand;
-		System.out.println("サイコロの目は" + num + "です");
-	}
-
-}
